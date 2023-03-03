ca081c55cb5b3c2397c32aea314dff1763b7a022
Abdullah ERTÜRK <54866587+abdullah-erturk@users.noreply.github.com>
2023-03-03 12:16:26 +0300

    Update RustDesk_Silent.bat

--- RustDesk_Silent.bat
+++ RustDesk_Silent.bat
@@ -24,7 +24,7 @@ echo.
 echo.
 
 :: Rustdesk software will be downloaded and installed as an overnight release only.
-:: You may need to manually change the github link in the script when Rustdesk releases the stable version of the software..
+:: You may need to manually change the github link in the script when Rustdesk releases the stable version of the software.
 echo    RustDesk downloading software, please wait...
 start /wait /min powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/rustdesk/rustdesk/releases/download/nightly/rustdesk-1.2.0-x86_64-pc-windows-msvc.exe', 'rustdesk%domain%,key=%key%.exe')" >nul 2>&1
 
