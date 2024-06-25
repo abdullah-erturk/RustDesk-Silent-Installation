e947b261000f4798c4f07de298649b117ad24956
Abdullah ERTÜRK <54866587+abdullah-erturk@users.noreply.github.com>
2024-06-25 15:45:58 +0300

    Update RustDesk_Silent.bat
    Updated script to download stable version of Rustdesk

--- RustDesk_Silent.bat
+++ RustDesk_Silent.bat
@@ -1,11 +1,34 @@
 
+
+' INFO:
+
+' RustDesk Silent Installation
+' Copyright (C) 2024 Abdullah ERTÜRK
+' https://github.com/abdullah-erturk/RustDesk-Silent-Installation
+
+
 @echo off
-title RustDesk
+setlocal enabledelayedexpansion
+title RustDesk ^| made by Abdullah ERTURK
 mode con:cols=50 lines=5
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit >nul 2>&1
-title RustDesk
 mode con:cols=95 lines=20
 
+if exist "%windir%\system32\curl.exe" (
+goto :run
+) else (
+echo.
+echo    ATTENTION
+echo.
+echo    curl.exe not found, first you need to download curl.exe and copy it to %windir%\system32\
+echo.
+echo Press any key to exit.
+pause >nul
+start "" "https://curl.se/windows/"
+exit
+)
+
+:run
 :: If you have a Rustdesk server, type the domain name or ip address. Example: set domain=-host=192.168.1.1
 set domain=
 
@@ -13,8 +36,7 @@ set domain=
 set key=
 
 :: NOTE: 
-:: If you do not specify domain and key, RustDesk software will be installed with default settings.
-
+:: If you do not specify a domain and key, RustDesk will install with default settings and use public servers.
 
 cd /d %temp%
 echo.
@@ -23,24 +45,46 @@ echo    RustDesk
 echo.
 echo.
 
-:: Rustdesk software will be downloaded and installed as an nightly release only.
-:: You may need to manually change the github link in the script when Rustdesk releases the stable version of the software.
+:: The script only downloads the latest stable version of Rustdesk software
 echo    RustDesk downloading software, please wait...
-start /wait /min powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/rustdesk/rustdesk/releases/download/nightly/rustdesk-1.2.0-x86_64-pc-windows-msvc.exe', 'rustdesk%domain%,key=%key%.exe')" >nul 2>&1
+
+set github_user=rustdesk
+set github_repo=rustdesk
+
+set target_name_match=x86_64.exe
+
+for /f "tokens=1,* delims=:" %%A IN ('curl -ks https://api.github.com/repos/%github_user%/%github_repo%/releases/latest ^| findstr "browser_download_url"') do (
+    set url=%%B
+    if not "!url:%target_name_match%=!"=="!url!" (
+        curl -kOL !url! >nul 2>&1
+
+        rem Dosyanın tam adını bulma
+        for /r %%F in (*%target_name_match%*) do (
+            set "downloaded_file=%%~nxF"
+            set "downloaded_file_path=%%F"
+        )
+
+        rem Geçici dosya adını oluşturma
+        set "new_filename=rustdesk%domain%,key=%key%.exe"
+
+        rem İndirilen dosyayı isimlendirme
+        move "!downloaded_file_path!" "!new_filename!" >nul 2>&1
+    )
+)
 
 echo.
 echo    Installing RustDesk software, please wait...
 start /wait rustdesk%domain%,key=%key%.exe --silent-install
 echo.
 echo.
-echo   !!! INSTALLATION COMPLETED !!!
+echo    INSTALLATION COMPLETED 
 echo.
 echo.
 echo    You can run the program via the shortcut created on the desktop or the Start Menu...
-del /f /q "rustdesk%domain%,key=%key%.exe" >nul
 echo.
 echo.
 echo    Press any key to EXIT...
 echo.
 pause >nul
+del /f /q "rustdesk%domain%,key=%key%.exe" >nul
 exit
